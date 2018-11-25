d7072cea91582d670b326b1b7fad91dec7bb3615
Quentin <qgerard.gerard@gmail.com>
2018-11-25 19:09:48 +0200

    jscript.form

--- src/js/jscript.form.js
+++ src/js/jscript.form.js
@@ -31,3 +31,40 @@ jscript.form.formToXML = function(inForm, inRootElement) {
     outXML += "</" + inRootElement + ">";
     return outXML;
 }
+
+jscript.form.selectLocateOption = function(inSelect, inValue, inJustFind, inCaseInsensitive) {
+    if (inSelect == null || 
+        inValue == null || inValue == "" ||
+        inCaseInsensitive == null ||
+        inJustFind == null) {
+            return;
+        }
+    if (inCaseInsensitive) {
+        inValue = inValue.toLowerCase();
+    }
+    var found = false;
+    var i;
+    for (i = 0; (i < inSelect.length) && !found; i++) {
+        var nextVal = inSelect.options[i].value;
+        if (inCaseInsensitive) {
+            nextVal = nextVal.toLowerCase();
+        }
+        if (nextVal == inValue) {
+            found = true;
+            if (!inJustFind) {
+                inSelect.options[i].selected = true;
+            }
+        }
+    }
+    return found;
+}
+
+jscript.form.selectSelectAll = function(inSelect) {
+    if (inSelect == null || !inSelect.options || inSelect.options.length == 0) {
+        return;
+    }
+    var i;
+    for (i = 0; i < inSelect.options.length; i++){
+        inSelect.options[i].selected = true;
+    }
+}
